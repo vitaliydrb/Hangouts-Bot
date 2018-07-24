d221c4100385d0222978a125010a714048812ee5
vitaliydrb <34272069+vitaliydrb@users.noreply.github.com>
2018-07-25 02:18:47 +0300

    Create basic bot logic

--- ctf.py
+++ ctf.py
@@ -1,9 +1,23 @@
 import datetime
-
-
+from dateutil import parser
+from bot import send_message
 class CTF:
 
     def __init__(self, name, link, beginning):
         self.name = ''
         self.link = ''
         self.beginning = ''
+        self.date = parser.parse(self.beginning)
+            
+    def get_eta(self):
+            
+        if self.date.year == datetime.date.today().year:
+            if self.date.date < 23:
+                if self.date.month == datetime.date.today().month:
+                    send_message(self)
+
+
+
+
+
+
