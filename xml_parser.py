af77389eca6fd5b28ead1c7c2942db709eb03c71
Vitaliy Mechytashvili <vim@underdefense.com>
2018-07-27 12:15:32 +0300

    Create xml-parser

--- xml_parser.py
+++ xml_parser.py
@@ -1,23 +1,35 @@
 import xml.etree.ElementTree as ET
-import requests
 import httplib2
+from ctf import CTF
 
 
 def get_rss():
     req = httplib2.Http()
     (headers, content) = req.request('https://ctftime.org/event/list/upcoming/rss/', 'GET')
     if headers['status'] == '200':
-        xml = parse_rss()
+        xml = parse_rss(content)
+        root = ET.fromstring(xml)
+        print root
+        return get_ctf_list(root)
 
 
 def parse_rss(content):
     content.replace('&lt;', '<')
     content.replace('&gt;','>')
     content.replace('&amp;','&')
-    content.replace('mdash;','—')
-    content.replace('ndash;','–')
+    content.replace('mdash;','-')
+    content.replace('ndash;','-')
     content.replace('&nbsp;',' ')
     return content
 
-if __name__ == '__main__':
-    get_rss()
+
+def get_ctf_list(xml):
+    events=[]
+    for item in xml[0].findall('item'):
+        title = item.find('title').text
+        link = item.find('url').text
+        beggining = item.find('start_date').text
+        event = CTF(title, link, beggining)
+        if event.get_eta():
+            events.append(event)
+    return events
