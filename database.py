c952e57c55960266b19c844256c032e56bc766dd
Vitaliy Mechytashvili <vim@underdefense.com>
2018-07-31 20:10:27 +0300

    Add functionality for reminding events, and checking in whitelist

--- database.py
+++ database.py
@@ -1,24 +1,27 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
+import datetime
+
 
 Base = declarative_base()
 #This class will be used as table markup.
 class Events(Base):
     __tablename__ = 'events'
     id = Column(Integer, primary_key = True)
-    event_name = Column(String(256))
-    event_date = Column(String(10))
-    thread_id = Column(String(1024))
+    name = Column(String(256))
+    date = Column(Date)
+    thread = Column(String(1024))
     
 
 def create_table():
     engine = create_engine('sqlite:///ctfs.db')
     Base.metadata.create_all(engine)
     return engine
+
 
 #This will check on import if database exist. And create it if not.
 if not os.path.exists('ctfs.db'):
@@ -31,7 +34,26 @@ def add_item(ctf, thread):
     Base.metadata.bind = engine
     DBsession = sessionmaker(bind = engine)
     session = DBsession()
-    new_ctf = Events(name=ctf.name, date='{} {} {}'.format(ctf.date.year, ctf.date.month, ctf.date.day), thread_id=thread)
+    new_ctf = Events(name=ctf.name, date=str(ctf.date.year + ' ' + ctf.date.month + ' ' +  ctf.date.day), thread=thread)
     session.add(new_ctf)
     session.commit()
 
+
+#This function will return list of events, should be reminded
+def get_upcoming_events():
+    engine = create_engine('sqlite:///ctfs.db')
+    Base.metadata.bind = engine
+    DBsession = sessionmaker(bind = engine)
+    session = DBsession()
+    tomorrow = datetime.date.today() + datetime.timedelta(days = 1)
+    items = session.query(Events).filter(Events.date == (tomorrow))
+    return items
+
+
+#This function checks for event, that already in chat-room.
+def is_unique(name):
+    engine = create_engine('sqlite:///ctfs.db')
+    Base.metadata.bind = engine
+    DBsession = sessionmaker(bind = engine)
+    session = DBsession()
+    return session.query(Events).filter(Events.name == name).count() == 0
